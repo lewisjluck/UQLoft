eaec3636bd3daf9bc63840dfd100fe947d85a238
Lewis Luck <lewisjluck@gmail.com>
2021-09-12 04:42:15 +1000

    Now that was BIG!

--- app.py
+++ app.py
@@ -1,38 +1,110 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect
 import requests
 from flask_cors import CORS
-from helpers import is_valid_course, get_ecp_details, get_paper_data
+from helpers import is_valid_course, get_ecp_details, get_paper_data, get_number_questions, update_questions, get_data
+import os
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/get_courses", methods = ["GET", "POST"])
-def get_courses():
-    return "hello"
+#Default error response
+def error(message="Something went wrong! Let's try that again."):
+    return render_template("error.html", message=message)
 
-@app.route("/is_course_valid", methods = ["GET", "POST"])
-def is_course_valid():
-    code = request.json["courseCode"]
-    return jsonify(is_valid_course(code))
+@app.route("/", methods = ["GET", "POST"])
+def homepage():
+    if request.method == "GET":
+        return render_template("homepage.html")
+    else:
+        course_code = request.form["course_code"].lower()
+        print(course_code)
+        print(is_valid_course(course_code))
+        if is_valid_course(course_code):
+            return redirect("/loft?course=" + course_code)
+        else:
+            return error("Hmmm, looks like we don't have that course yet!")
 
+@app.route("/loft", methods = ["GET", "POST"])
+def loft():
+    if request.method == "GET":
+        code = request.args.get("course")
+        url, course_name, course_description = get_ecp_details(code)
+        print(course_name, course_description)
+        course_name = " ".join(course_name.split()[:-1])
+        papers = get_paper_data(code)
+        message_papers = [(f"/paper?det={code}_{paper['year']}_sem{paper['semester']}" ,f"{paper['year']} Semester {paper['semester']}") for paper in papers]
+        print(message_papers)
+        return render_template("loft.html", url=url, course_code=code, course_name=course_name, course_description=course_description, papers=message_papers)
 
-@app.route("/get_course_info", methods = ["GET", "POST"])
-def get_papers():
-    code = request.json["courseCode"]
-    url, course_name, course_description = get_ecp_details(code)
-    papers = get_paper_data(code)
-    data = {
-        "ecp_link" : url,
-        "course_name": course_name,
-        "course_description": course_description,
-        "papers": papers
-    }
-    return jsonify(data)
+@app.route("/paper", methods = ["GET", "POST"])
+def papers():
+    code = request.args.get("det")
+    file = f"../static/papers/{code}.pdf"
+    course, year, sem = code.split('_')
+    paper_description = f"{course.upper()} - {year}, Semester {sem[-1]}"
+    question_num = get_number_questions(course.lower(), int(year), int(sem[-1]))
+    if request.method == "GET":
+        return render_template("papers.html", code=code, file=file, description=paper_description, num=question_num)
+    else:
+        new_question_data = []
+        for i in range(question_num):
+            new_question_data.append(request.form.get(str(i+1)))
+        print(new_question_data)
+        update_questions(code, new_question_data)
+        return redirect("/solutions?paper=" + code)
 
-@app.route("/get_paper", methods = ["GET", "POST"])
-def get_paper():
-    course_code = request.json["courseCode"]
-    paper = request.json["paper"]
-    print(coure_code, paper)
-    return jsonify("YOLO")
+@app.route("/solutions", methods = ["GET", "POST"])
+def solutions():
+    code = request.args.get("paper")
+    course, year, sem = code.split('_')
+    paper_description = f"{course.upper()} - {year}, Semester {sem[-1]}"
+    data = get_data(code)
+    return render_template("solutions.html", code=code, description=paper_description, data=data)
+
+@app.route("/courses", methods = ["GET", "POST"])
+def courses():
+    return render_template("courses.html")
+
+@app.route("/about", methods = ["GET", "POST"])
+def about():
+    return render_template("about.html")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+#
+# @app.route("/is_course_valid", methods = ["GET", "POST"])
+# def is_course_valid():
+#     code = request.json["courseCode"]
+#     return jsonify(is_valid_course(code))
+#
+#
+# @app.route("/get_course_info", methods = ["GET", "POST"])
+# def get_papers():
+#     code = request.json["courseCode"]
+#     url, course_name, course_description = get_ecp_details(code)
+#     papers = get_paper_data(code)
+#     data = {
+#         "ecp_link" : url,
+#         "course_name": course_name,
+#         "course_description": course_description,
+#         "papers": papers
+#     }
+#     return jsonify(data)
+#
+# @app.route("/serve_paper", methods = ["GET", "POST"])
+# def serve_paper():
+#     file = "/papers/" + request.args.get("file")
+#     print(os.path.join(THIS_FOLDER, file))
+#     return send_file(THIS_FOLDER + file, mimetype="application/pdf", cache_timeout=0)
+
+if __name__ == '__main__':
+    os.environ['FLASK_ENV'] = 'development'
+    app.run(debug=True)
--- helpers.py
+++ helpers.py
@@ -21,7 +21,6 @@ def close_db(db):
 #Check if any papers have this course title
 def is_valid_course(query_course_code):
     db = open_db()
-    matches = []
     papers =  db.execute("SELECT * FROM papers WHERE course_code LIKE ?", ("%" + query_course_code.lower() + "%",)).fetchall()
     if papers:
         return True
@@ -31,49 +30,61 @@ def is_valid_course(query_course_code):
 #Get paper with this course title
 def get_paper_data(query_course_code):
     db = open_db()
-    matches = []
     papers =  db.execute("SELECT * FROM papers WHERE course_code LIKE ?", ("%" + query_course_code.lower() + "%",)).fetchall()
     paper_list = []
     for paper in papers:
         paper_list.append({"course": paper[1], "year": paper[2], "semester": paper[3]})
     return paper_list
 
+def get_number_questions(course, year, semester):
+    db = open_db()
+    matches = []
+    papers =  db.execute("SELECT * FROM papers WHERE course_code LIKE ?", ("%" + course.lower() + "%",)).fetchall()
+    for paper in papers:
+        if paper[1] == course and paper[2] == year and paper[3] == semester:
+            return paper[4]
+
 def get_ecp_details(course_code):
     url = "https://my.uq.edu.au/programs-courses/course.html?course_code=" + course_code.upper()
     if course_code.lower() == "csse1001":
-        return (url, 'Introduction to Software Engineering (CSSE1001)', '''Introduction to Software Engineering through programming with particular focus
+        return (url, 'Introduction to Software Engineering', '''Introduction to Software Engineering through programming with particular focus
 on the fundamentals of computing & programming, using an exploratory problem-based approach. Building abstractions with procedures, data & objects; data modelling; desig
 ning, coding & debugging programs of increasing complexity''')
-    url = "https://my.uq.edu.au/programs-courses/course.html?course_code=" + course_code.upper()
+    elif course_code.lower() == "csse2002":
+        return (url, "Programming in the Large", '''Working on large and complex software systems and ensuring those systems
+remain maintainable requires disciplined, individual practices. Software must be well-specified, well-impleme
+nted and well-tested. This course covers concepts and techniques in modern programming languages that help su
+pport good practice (such as OO concepts, genericity and exception handling) with specific application to fil
+e IO and GUIs in Java.''')
     session = HTMLSession()
-
     r = session.get(url)
     course_name = r.html.find('#course-title', first=True).text
     course_description = r.html.find('#course-summary', first=True).text
     return (url, course_name, course_description)
 
-#Add product lot
-def add_lot(product):
+#id - paper_code - question_num - a,b,c,d,e_count
+
+def update_questions(paper_code, data):
     db = open_db()
-    db.execute("INSERT INTO lots (ref, lot, info) VALUES (?, ?, ?)", (product.reference, product.lot, date.today().strftime("%d/%m/%Y")))
+    for (i, answer) in enumerate(data):
+        res = db.execute("SELECT * FROM questions WHERE paper_code=? AND question_num=?", (paper_code,i+1)).fetchone()
+        if res:
+            res = list(res)
+            if answer:
+                res[ord(answer) - 62] += 1
+                db.execute("UPDATE questions SET a = ?, b = ?, c = ?, d = ?, e = ? WHERE paper_code = ? AND question_num = ?", (res[3], res[4], res[5], res[6], res[7], paper_code, i+1))
+        else:
+            update = [0, 0, 0, 0, 0]
+            if answer:
+                update[ord(answer) - 65] += 1
+            db.execute("INSERT INTO questions (paper_code, question_num, a, b, c, d, e) VALUES (?, ?, ?, ?, ?, ?, ?)", (paper_code, i+1, *update))
     db.commit()
     close_db(db)
 
-#Update databse information depending on input data
-def update_product(product):
+def get_data(code):
     db = open_db()
-    response = {"new":False, "lot_changed":False, "reference":product.reference}
-    search_product = db.execute("SELECT * FROM products WHERE ref=?", (product.reference,)).fetchone()
-    if search_product:
-        db.execute("UPDATE products SET description = ? WHERE ref=?", (product.description, product.reference))
-    else:
-        db.execute("INSERT INTO products (ref, description) VALUES (?, ?)", (product.reference, product.description))
-        response["new"] = True
-    sql_lots = db.execute("SELECT * FROM lots WHERE ref=?", (product.reference,)).fetchall()
-    lots = [lot[0] for lot in sql_lots]
-    if not product.lot in lots and not product.lot == "":
-        db.execute("INSERT INTO lots (ref, lot, info) VALUES (?, ?, ?)", (product.reference, product.lot, date.today().strftime("%d/%m/%Y")))
-        response["lot_changed"] = True
-    db.commit()
+    res = db.execute("SELECT * FROM questions WHERE paper_code=?", (code,)).fetchall()
     close_db(db)
-    return response
+    return res
+
+#update_questions("csse1001_2019_sem2", ['B', 'C', 'B', 'A', 'C', None, 'D', 'E', 'B', 'B', 'A', 'C', 'D', 'E', 'B', 'C', 'C', 'D', 'D', None, 'A', None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None])
